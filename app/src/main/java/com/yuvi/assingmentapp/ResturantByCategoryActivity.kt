4e3ba2d116d217f7d4b2c828eac52c64708347f5
yuvaraj <yuvaraj@interactionone.com>
2021-01-17 21:49:59 +0530

    Implementation -> Search restaurant By Catgory

--- app/src/main/java/com/yuvi/assingmentapp/ResturantByCategoryActivity.kt
+++ app/src/main/java/com/yuvi/assingmentapp/ResturantByCategoryActivity.kt
@@ -1,46 +1,78 @@
 package com.yuvi.assingmentapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
+import com.yuvi.assingmentapp.adapter.CategoryAdapter
+import com.yuvi.assingmentapp.adapter.RestaurantAdapter
 import com.yuvi.assingmentapp.constants.DataConstants
 import com.yuvi.assingmentapp.model.CategoriesList
+import com.yuvi.assingmentapp.model.Restaurants
+import com.yuvi.assingmentapp.model.ResturantByCategoryApiResponse
 import com.yuvi.assingmentapp.view_model.CategoriesListViewModel
 import com.yuvi.assingmentapp.view_model.ResturantByCategoryViewModel
+import com.yuvi.assingmentapp.views.MainActivity
+import java.util.ArrayList
 
 class ResturantByCategoryActivity : AppCompatActivity() {
+
+    var catId: String? =null
+    private val restaurantsDataList: ArrayList<Restaurants> = ArrayList<Restaurants>()
+    private lateinit var restaurantAdapter: RestaurantAdapter
+    private var mContext : Context? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_restaurant)
+        mContext = this@ResturantByCategoryActivity
+        catId= intent.getStringExtra("categoryId")
+
+  //      Toast.makeText(this,catId,Toast.LENGTH_LONG).show()
+
+
+        val recyclerView: RecyclerView = findViewById(R.id.recyclerview)
+        restaurantAdapter = RestaurantAdapter(mContext as ResturantByCategoryActivity,restaurantsDataList)
+        val layoutManager = LinearLayoutManager(applicationContext)
+        recyclerView.layoutManager = layoutManager
+        recyclerView.itemAnimator = DefaultItemAnimator()
+        recyclerView.adapter = restaurantAdapter
+
         fetchResturantByCategory()
     }
 
     fun fetchResturantByCategory ()
     {
-        val categoryViewModel: ResturantByCategoryViewModel = ViewModelProviders.of(this).get(
+        val resturantViewModel: ResturantByCategoryViewModel = ViewModelProviders.of(this).get(
             ResturantByCategoryViewModel::class.java)
 
-        categoryViewModel.initialize(DataConstants.lat?.toDouble(),this,DataConstants.longti?.toDouble(),"1")
-        categoryViewModel.getCategoriesList()?.observe(this, androidx.lifecycle.Observer {
+        resturantViewModel.initialize(DataConstants.lat?.toDouble(),this,DataConstants.longti?.toDouble(),catId.toString())
+        resturantViewModel.getCategoriesList()?.observe(this, androidx.lifecycle.Observer {
             if (it!=null)
             {
 
-              Log.e("Check" , "resturant===>   "+it.restaurants)
+              Log.e("Check" , "resturant===>   "+it.restaurants[0].restaurant.id)
 
-//                it.categorieslist.forEach {
-//                    it.categories.name
-//                    Log.e("CategoryList" , ""+it.categories.name)
-//                }
-//                Log.e("Check" , it.categorieslist[0].categories.name)
-//
-//                val categoryList: List<CategoriesList> = it.categorieslist
-//                Log.e("Checked" , it.categorieslist[0].categories.name)
+                it.restaurants.forEach {
+                    it.restaurant.id
 
-//                CategoryDataList.addAll(categoryList)
-//
-//                categoryAdapter.notifyDataSetChanged()
+                }
+
+if(it.restaurants.size !=0)
+{
+    val restList: List<Restaurants> = it.restaurants
+
+
+    restaurantsDataList.addAll(restList)
+
+    restaurantAdapter.notifyDataSetChanged()
+}
+
             }else
             {
                 Log.i("Status" , "Failed")
--- app/src/main/java/com/yuvi/assingmentapp/adapter/RestaurantAdapter.kt
+++ app/src/main/java/com/yuvi/assingmentapp/adapter/RestaurantAdapter.kt
@@ -8,42 +8,51 @@ import android.view.ViewGroup
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.annotation.NonNull
-import androidx.databinding.DataBindingUtil
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 import com.yuvi.assingmentapp.R
 import com.yuvi.assingmentapp.ResturantByCategoryActivity
 import com.yuvi.assingmentapp.model.CategoriesList
-import com.yuvi.assingmentapp.model.CategoriesResponse
+import com.yuvi.assingmentapp.model.Restaurants
 
 
-
-
-internal class CategoryAdapter(var mcontext : Context, private var categoryList: List<CategoriesList>) :
-    RecyclerView.Adapter<CategoryAdapter.MyViewHolder>() {
+internal class RestaurantAdapter(var mcontext : Context, private var restaurantList: List<Restaurants>) :
+    RecyclerView.Adapter<RestaurantAdapter.MyViewHolder>() {
     internal inner class MyViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         var title: TextView = view.findViewById(R.id.category_name)
-        var category_id: TextView = view.findViewById(R.id.category_id)
+        var rest_id: TextView = view.findViewById(R.id.category_id)
         var imageView: ImageView = view.findViewById(R.id.image)
 
     }
     @NonNull
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
         val itemView = LayoutInflater.from(parent.context)
-            .inflate(R.layout.category_item, parent, false)
+            .inflate(R.layout.item_restuarant, parent, false)
         return MyViewHolder(itemView)
     }
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        val category = categoryList[position]
-        holder.title.text =categoryList[position].categories.name
-        holder.category_id.text = categoryList[position].categories._id.toString()
+        val restaurant = restaurantList[position]
+        holder.title.text = restaurant.restaurant.name
+        holder.rest_id.text = restaurant.restaurant.id
+
+        if (restaurant.restaurant.featured_image !== null) {
+            Glide.with(mcontext)
+                .load(restaurant.restaurant.featured_image)
+                .into( holder.imageView)
+        }
+        else
+        {
+
+        }
+
         holder.imageView.setOnClickListener {
-            val intent = Intent(mcontext, ResturantByCategoryActivity::class.java)
-            intent.putExtra("categoryId",holder.category_id.text.toString())
-            mcontext.startActivity(intent)
+//            val intent = Intent(mcontext, ResturantByCategoryActivity::class.java)
+//            intent.putExtra("categoryId",)
+//            mcontext.startActivity(intent)
         }
 
     }
     override fun getItemCount(): Int {
-        return categoryList.size
+        return restaurantList.size
     }
 }
